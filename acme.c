322e7eb0ffc2cbc6eaa818b619c911dd77b90cc9
Chad Coates <ccoates@nrgsim.com>
2017-05-28 18:22:51 -0700

    wtf

--- acme.c
+++ acme.c
@@ -11,31 +11,65 @@
 
 static dev_t acme_dev_number;
 static struct class *acme_class;
-static struct timer_list acme_timer;
+//static struct timer_list acme_timer;
 //struct *acme_devp;
 static int blink_rate=BLINKRATE;
 
 module_param(blink_rate,int,S_IRUGO|S_IWUSR);
 
+/*
 static void acme_timer_cb(unsigned long data){
 	acme_devp->led_ctl = readl(acme_devp->hw.led_addr);	
 	acme_devp->led_ctl=(acme_devp->led_ctl==LED_OFF) ? LED_ON : LED_OFF;
 	writel(acme_devp->led_ctl,acme_devp->hw.led_addr);
 	mod_timer(&acme_timer,(jiffies+(HZ/acme_devp->blink_rate)/2));
 }
+//*/
+/*
+static int __devinit ixgbe_probe(){
+//	INIT_WORK(&adapter->service_task,ixgbe_service_task);
+}
+//*/
 
+/*
+static void __dev_exit(){
+//	cancel_work_sync(&adapter->service_task);
+}
+//*/
+
+//*
+static irqreturn_t acme_irq_handler(int irq, void *data){
+//	struct acme_data_t *acme_data=data;
+	u32 cause;
+	acme_devp->irq_info=irq;
+	cause=readl(acme_devp->hw.hw_addr+ICR);
+	switch(cause){
+		case REG_IRQ_RX:
+//			schedule_work(acme_data->io_task);
+			break;
+		default:
+			break;
+	}
+	return IRQ_HANDLED;
+}
+//*/
 static int acme_open(struct inode *inode,struct file *filp){
 	int  err=0;
-	setup_timer(&acme_timer, acme_timer_cb, (unsigned long)&acme_devp);
-	writel(LED_ON,acme_devp->hw.led_addr);
-	mod_timer(&acme_timer,(jiffies+(HZ/acme_devp->blink_rate)/2));
+//	void *data=NULL;
+	struct pci_dev *pdev=acme_devp->pdev;
+	err=request_irq(pdev->irq,acme_irq_handler,0,"acme_int",NULL);
+
+	if(err)err=-EBUSY;
+//	setup_timer(&acme_timer, acme_timer_cb, (unsigned long)&acme_devp);
+//	writel(LED_ON,acme_devp->hw.led_addr);
+//	mod_timer(&acme_timer,(jiffies+(HZ/acme_devp->blink_rate)/2));
 	return err;
 }
 
 static int acme_close(struct inode *inode,struct file *filp){
 	int err=0;
-	writel(LED_OFF,acme_devp->hw.led_addr);
-	del_timer_sync(&acme_timer);
+//	writel(LED_OFF,acme_devp->hw.led_addr);
+//	del_timer_sync(&acme_timer);
 	return err;
 }
 
@@ -92,50 +126,77 @@ static const struct file_operations acme_fops = {
 	.write 		= acme_write,
 };
 
-static int descriptor_init(struct pci_dev *pdev){
-	struct acme_ring *rx_ring = acme_devp->rx_ring;
-	int i=0;
-/*
-	if(!rx_ring->count)rx_ring->count=ACME_RXD;
-
-	rx_ring->buffer_info=kcalloc(rx_ring->count,sizeof(struct acme_buffer),GFP_KERNEL);
-
-	if (!rx_ring->buffer_info) {
-	//	ret_val = 5;
-		goto err_nomem;
-	}
-	
-	rx_ring->size = rx_ring->count * sizeof(struct acme_desc);
-	rx_ring->desc = dma_alloc_coherent(&pdev, rx_ring->size,
-									   &rx_ring->dma, GFP_KERNEL);
-
-	if (!rx_ring->desc) {
-		ret_val = 6;
-		goto err_nomem;
-	}
-	rx_ring->next_to_use = 0;
-	rx_ring->next_to_clean = 0;
-*/
-
-	do{
-		acme_devp->rx_desc[i]=ACME_RX_DESC(*rx_ring,i);
-		acme_devp->rx_desc[i]->buffer_addr=cpu_le64(rx_ring->buffer_info->dma);
-//		acme_devp->rx_desc[i]->lower.data=cpu_to_le32(rxd_lower|buffer_info->length);
-//		acme_devp->rx_desc[i]->upper.data=cpu_to_le32(rxd_upper);
-		++i;
-		if(i==rx_ring->count)i=0;
-	}while(--count>0);
-
-
-
+static int acme_alloc_ring_dma(struct acme_ring *ring, struct pci_dev *pdev){
+	ring->desc=dma_alloc_coherent(&pdev->dev,ring->size,&ring->dma,GFP_KERNEL);
+	writel((ring->dma>>32) & 0xFFFFFFFF,acme_devp->hw.hw_addr+RDBAH0);
+	writel(ring->dma & 0xFFFFFFFF,acme_devp->hw.hw_addr+RDBAL0);
+	if(!ring->desc)return -ENOMEM;
 	return 0;
 }
 
-static int descriptor_free(void){
-	int i=0;
-	for (;i<ACME_RXD;++i){
-//		kfree(desc[i].buff_addr);
+static int rx_ring_init(struct pci_dev *pdev){
+	struct acme_ring *rx_ring = acme_devp->rx_ring;
+	int i,err=-ENOMEM;
+	
+	rx_ring->count=ACME_RXD;
+
+	rx_ring->size=rx_ring->count*sizeof(struct acme_rx_desc);
+
+	err=acme_alloc_ring_dma(rx_ring,pdev);
+	if(err)goto err;
+
+	for (i=0;i<rx_ring->count;++i) {
+		rx_ring->wtf[i]=(u8 *)dma_alloc_coherent(&pdev->dev,DESC_SIZE,&rx_ring->handle[i],GFP_KERNEL);
+		if (!rx_ring->wtf[i]) goto err_pages;
+//		if(i==rx_ring->count-2)
+//			writel(buffer_info->dma,acme_devp->hw.hw_addr+RDT0);
+//		if(i==rx_ring->count-1)
+//			writel(buffer_info->dma,acme_devp->hw.hw_addr+RDT0);
 	}
+	
+	writel(DESC_SIZE,acme_devp->hw.hw_addr+RDLEN0);
+
+	writel(0x01,acme_devp->hw.hw_addr+CTRL);
+	
+	rx_ring->next_to_use=0;
+	rx_ring->next_to_clean=0;
+
+	return 0;
+err_pages:
+	for (i = 0; i < rx_ring->count; i++) {
+		kfree(rx_ring->wtf[i]);
+	}
+	return 0;
+err:
+	vfree(rx_ring->buffer_info);
+	return err;
+}
+
+static void clean_rx_ring(struct acme_ring *rx_ring){
+	int i;
+	struct acme_buffer *buffer_info = rx_ring->buffer_info;
+	for (i = 0; i < rx_ring->count; i++) {
+		buffer_info = &rx_ring->buffer_info[i];
+		kfree(buffer_info->buffer);
+	}
+}
+
+static int rx_ring_free(void){
+	struct pci_dev *pdev = acme_devp->pdev;
+	struct acme_ring *rx_ring=acme_devp->rx_ring;
+	int i;
+
+	clean_rx_ring(rx_ring);
+
+	for (i = 0; i < rx_ring->count; i++)
+		kfree(rx_ring->buffer_info[i].ps_pages);
+		
+	vfree(rx_ring->buffer_info);
+	rx_ring->buffer_info = NULL;
+
+	dma_free_coherent(&pdev->dev, rx_ring->size, rx_ring->desc,rx_ring->dma);
+	rx_ring->desc = NULL;
+
 	return 0;
 }
 
@@ -145,15 +206,55 @@ amce_pci_probe(struct pci_dev *pdev, const struct pci_device_id *ent){
 	resource_size_t mmio_start, mmio_len;
 
 	printk(KERN_INFO "It's dangerous to go alone, take this with you.\n");
-	
+
+
+	/*
+	//initialize the workqueue
+//	INIT_WORK(&ece_led->ece_work,ece_work_queue);
+
+	//disable interrupts
+//	WRITE_REG(ece_led,IMC,0xffffffff);
+		writel(0xFFFFFFFF,acme_devp->hw.hw_addr+IMC);
+//	FLUSH(ece_led);
+
+	//global reset
+//	WRITE_REG(ece_led,CTRL,(READ_REG(ece_led,CTRL) | CTRL_RST);
+		writel((readl(acme_devp->hw.hw_addr+CTRL) | CTRL_RST),acme_devp->hw.hw_addr+CTRL);
+//	FLUSH(ece_led);
+
+	//wait for the PHY reset
+	msleep(25);
+
+	//disable interrupts again ater reset
+//	WRITE_REG(ece_led,IMC,0xffffffff);
+	writel(0xFFFFFFFF,acme_devp->hw.hw_addr+IMC);
+//	FLUSH(ece_led);
+
+	// needed for PCIe workaraounds - reserved chicken bits
+	// write GCR bit 22, GCR2 bit 1
+	//
+//	WRITE_REG(ece_led,GCR,(READ_REG(ece_led,GCR) | (1<<22)));
+	writel((readl(acme_devp->hw.hw_addr+GCR) | (1<<22)),acme_devp->hw.hw_addr+GCR);
+//	WRITE_REG(ece_led,GCR2,(READ_REG(ece_led,GCR2) | 1));
+	writel((readl(acme_devp->hw.hw_addr+GCR2) | (1<<22)),acme_devp->hw.hw_addr+GCR2);
+
+	//needed for a forced PHY setup
+	//PHY setups
+//	WRITE_REG(ece_led,MDIC,0x1831af08);
+	writel(0x1831af08,acme_devp->hw.hw_addr+MDIC);
+	//*/
+
+
+	acme_devp->pdev=pdev;
+
 	err=pci_enable_device_mem(pdev);
-if(err)return err;
+	if(err)return err;
 
 	bars=pci_select_bars(pdev, IORESOURCE_MEM);
 	
 	err=pci_request_selected_regions(pdev,bars,DEVNAME);
 	if(err)goto err_pci_reg;
-	descriptor_init();
+	rx_ring_init(pdev);
 	pci_set_master(pdev);
 	
 	mmio_start = pci_resource_start(pdev, 0);
@@ -167,7 +268,7 @@ err_pci_reg:
 }
 
 static void amce_pci_remove(struct pci_dev *pdev){
-	descriptor_free();
+	rx_ring_free();
 	pci_release_selected_regions(pdev,pci_select_bars(pdev,IORESOURCE_MEM));
 	//pci_release_mem_regions(pdev);
 	pci_disable_device(pdev);
@@ -223,6 +324,9 @@ static int __init amce_init(void){
 }
 
 static void __exit amce_exit(void){
+//	WRITE_REG(ece_led,IMC,0xffffffff);
+//	WRITE_REG(ece_led,RCTL,(READ_REG(ece_led,RCTL) & ~RCTL_EN);
+	
 	cdev_del(&acme_devp->cdev);
 	unregister_chrdev_region(acme_dev_number,DEVCOUNT);
 	kfree(acme_devp);
@@ -238,3 +342,21 @@ MODULE_VERSION("0.5");
 
 module_init(amce_init);
 module_exit(amce_exit);
+//*/
+/*// e1000_tx_queue
+ 	
+	do{
+		buffer_info=&rx_ring->buffer_info[i];
+
+		rx_desc=ACME_RX_DESC(*rx_ring,i);
+		rx_desc->buff_addr=cpu_to_le64(rx_ring->buffer_info->dma);
+		rx_desc->lower.data=cpu_to_le32(rxd_lower|buffer_info->length);
+		rx_desc->upper.data=cpu_to_le32(rxd_upper);
+		++i;
+		if(i==rx_ring->count)i=0;
+	}while(--count>0);
+
+	rx_desc->lower.data |= cpu_to_le32(acme_devp->rxd_cmd);
+	writel(i,acme_devp->hw.hw_addr+TDT);
+//*/
+
--- acme.h
+++ acme.h
@@ -9,7 +9,10 @@
 #include <linux/types.h>
 #include <linux/skbuff.h>
 #include <linux/if.h>
-//#include <linux/slab.h>
+#include <linux/delay.h>
+#include <linux/vmalloc.h>
+#include <linux/interrupt.h>
+#include <linux/kernel.h>
 #include <asm/uaccess.h>
 
 #define DEVCOUNT					1
@@ -20,8 +23,33 @@
 #define LEDCTL 						0x00E00
 #define ACME_RXD	 				0x10	//number of descriptors
 #define DESC_SIZE					0x800 // 2048 bytes
+#define REG_IRQ_RX				0x0001
+#define TDT								0x03818
+#define TDH								0x03810
+#define ICR								0x000C0
+#define RDBAL0						0x02800
+#define RDBAH0						0x02804
+#define RDLEN0						0x02808
+#define RDH0							0x02810
+#define RDT0							0x02818
+#define RCTL							0x00100
+#define CTRL							0x00000
+#define IMC								0x000D8
+#define GCR								0x05B00
+#define GCR2							0x05B64
+#define MDIC							0x00020
+#define CTRL_RST					(1<<26)
+#define IFCS							0x02000000
 
-static void acme_timer_cb(unsigned long);
+#define ACME_GET_DESC(R, i, type) (&(((struct type *)((R).desc))[i]))
+#define ACME_RX_DESC(R, i)	ACME_GET_DESC(R, i, acme_rx_desc)
+
+//static void acme_timer_cb(unsigned long);
+
+struct ixgbe_adapter{
+	struct work_struct service_task;
+};
+
 
 struct pci_hw{
 	void __iomem *hw_addr;
@@ -29,30 +57,36 @@ struct pci_hw{
 };
 
 struct acme_dev{
-	struct cdev cdev;				
+	struct cdev cdev;
+	struct pci_dev *pdev;
 	struct pci_hw hw;
+	u32 rxd_cmd;
 	struct acme_ring *rx_ring;
+ 	struct acme_tx_desc *rx_desc[ACME_RXD];
 	struct acme_ring *tx_ring;
- 	struct acme_desc desc[ACME_RXD];
-	u32 led_ctl;    
+	u32 led_ctl;
+	int irq_info;
 	int blink_rate; //blinks per second
 } *acme_devp;
-
-struct acme_data{
-	void *dma_buffer_rx;
-	void *dma_buffer_tx;
-dma_addr_t dma_rx;
-	dma_addr_t dma_tx;
-} *dev_data;
 
 struct acme_page{
 	struct page *page;
 	u64 dma;
 };
 
+// alloc_ring_dma where
+// tx_map
 struct acme_buffer{
+
+	// physical address back from dma_map_single
 	dma_addr_t dma;
-	struct sk_buff *skb;
+	void *buffer;
+	// skb has a data field that is the payload 
+	// which is being sent to dma_map_single
+	// pinning so we can dma it when we get the
+	// pysical address back for dma_map_single
+	// it is assigned to dma
+	struct sk_buff *skb;// points to kernal logical
 	union {
 		/* Tx */
 		struct {
@@ -72,7 +106,39 @@ struct acme_buffer{
 	};
 };
 
-struct acme_desc{
+struct acme_ring{
+	void *desc;
+	dma_addr_t dma;
+	u8 *wtf[ACME_RXD];
+	dma_addr_t handle[ACME_RXD];
+	unsigned int size;
+	unsigned int count;
+	u16 next_to_use;
+	u16 next_to_clean;
+	void __iomem *head;
+	void __iomem *tail;
+	struct acme_buffer *buffer_info;
+	char name[IFNAMSIZ+5];
+	u32 ims_val;
+	u32 itr_val;
+	void __iomem * itr_register;
+	int set_itr;
+	struct sk_buff *rx_skb_top;
+};
+
+
+struct acme_rx_desc{
+	__le64 buff_addr;
+	__le16 length;
+	__le16 csum;
+	u8 status;
+	u8 errors;
+	__le16 special;
+};
+
+
+//struct acme_rx_desc{
+struct acme_tx_desc{
 	__le64 buff_addr;
 	union{
 		__le32 data;
@@ -92,22 +158,5 @@ struct acme_desc{
 	}upper;
 };
 
-struct acme_ring{
-	void *desc;
-	dma_addr_t dma;
-	unsigned int size;
-	unsigned int count;
-	u16 next_to_use;
-	u16 next_to_clean;
-	void __iomem *head;
-	void __iomem *tail;
-	struct acme_buffer *buffer_info;
-	char name[IFNAMSIZ+5];
-	u32 ims_val;
-	u32 itr_val;
-	void __iomem * itr_register;
-	int set_itr;
-	struct sk_buff *rx_skb_top;
-};
 
 #endif
